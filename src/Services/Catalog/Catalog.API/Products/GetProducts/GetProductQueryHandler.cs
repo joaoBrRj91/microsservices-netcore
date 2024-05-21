02a4c43ce4473f65116b877e36b819874c325c27
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-05-21 19:51:38 -0300

    Implementation GetProductQueryHandler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal sealed class GetProductQueryHandler(
+    ILogger<GetProductQueryHandler> logger, IDocumentSession session)
+    : IQueryHandler<GetProductQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("{GetProductQueryHandler} called with {query}", nameof(GetProductQueryHandler), query);
+
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductsResult(products);
+    }
+}
