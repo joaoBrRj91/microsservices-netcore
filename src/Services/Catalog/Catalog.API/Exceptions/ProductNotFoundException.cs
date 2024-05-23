8cb736f7c47887573139201252559e2c8cf64813
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-05-23 19:09:44 -0300

    Create handler for get product by id

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Exceptions;
+
+public class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException() : base("Product not found") { }
+    public ProductNotFoundException(string message) : base(message) { }
+    public ProductNotFoundException(string message, Exception innerException) : base(message,innerException) { }
+
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Products.GetProducts;
+using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product Product);
+
+internal sealed class GetProductByIdQueryHandler(ILogger<GetProductByIdQueryHandler> logger,
+    IDocumentSession session)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("{GetProductByIdQueryHandler} called with {query}", nameof(GetProductByIdQueryHandler), query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if(product is null)
+            throw new ProductNotFoundException();
+    }
+}
