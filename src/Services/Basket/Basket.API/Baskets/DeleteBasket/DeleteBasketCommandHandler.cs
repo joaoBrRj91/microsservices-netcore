179ce6e44841f13ba49956a9af907d2e6845225d
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-07-09 20:02:27 -0300

    Delete Basket Feature Init

--- src/Services/Basket/Basket.API/Baskets/DeleteBasket/DeleteBasketCommandHandler.cs
+++ src/Services/Basket/Basket.API/Baskets/DeleteBasket/DeleteBasketCommandHandler.cs
@@ -1,5 +1,22 @@
-﻿namespace Basket.API.Baskets.DeleteBasket;
+﻿
+namespace Basket.API.Baskets.DeleteBasket;
 
-public class DeleteBasketCommandHandler
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(b => b.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+    {
+        //TODO : Delete basket from database and cache
+        return await Task.FromResult(new DeleteBasketResult(true));
+    }
 }
--- src/Services/Basket/Basket.API/Baskets/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Baskets/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,5 +1,28 @@
-﻿namespace Basket.API.Baskets.DeleteBasket;
+﻿
+using Basket.API.Baskets.GetBasket;
 
-public class DeleteBasketEndpoint
+namespace Basket.API.Baskets.DeleteBasket;
+
+//public record DeleteBasketRequest(string UserName);
+public record DeleteBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteBasketCommand(userName));
+
+            var response = result.Adapt<DeleteBasketResponse>();
+
+            return Results.Ok(response);
+        })
+         .WithName("DeleteBasket")
+         .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+         .ProducesProblem(StatusCodes.Status400BadRequest)
+         .ProducesProblem(StatusCodes.Status404NotFound)
+         .WithSummary("Delete Basket By UserName")
+         .WithDescription("Delete Basket of the user for ecommerce microsservices");
+    }
 }
