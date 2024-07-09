70641a4dca262336760eedac01f33db3300de34b
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-07-09 19:08:25 -0300

    Store Basket Feature Init

--- src/Services/Basket/Basket.API/Baskets/StoreBasket/StoreBasketCommandHandler.cs
+++ src/Services/Basket/Basket.API/Baskets/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,5 +1,29 @@
-﻿namespace Basket.API.Baskets.StoreBasket;
+﻿
+namespace Basket.API.Baskets.StoreBasket;
 
-public class StoreBasketCommandHandler
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(bool IsSuccess, string UserName);
+
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(b => b.Cart).NotEmpty().WithMessage("Cart can not be empty");
+        RuleFor(b => b.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
+    {
+
+        var cart = request.Cart;
+
+        //TODO : Store basket in database (use Marten upsert - if exist = update, if not create)
+        //TODO : Update Cache service with changes basket user 
+
+        return await Task.FromResult(new StoreBasketResult(true, cart.UserName));
+    }
 }
--- src/Services/Basket/Basket.API/Baskets/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Baskets/StoreBasket/StoreBasketEndpoint.cs
@@ -1,5 +1,30 @@
-﻿namespace Basket.API.Baskets.StoreBasket;
+﻿
+using Basket.API.Baskets.GetBasket;
 
-public class StoreBasketEndpoint
+namespace Basket.API.Baskets.StoreBasket;
+
+public record StoreBasketRequest(ShoppingCart Cart);
+public record StoreBasketResponse(bool IsSuccess, string UserName);
+
+public class StoreBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<StoreBasketCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<StoreBasketResponse>();
+
+            return Results.Created($"/basket/{response.UserName}", response);
+        })
+         .WithName("StoreBasket")
+         .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+         .ProducesProblem(StatusCodes.Status500InternalServerError)
+         .ProducesProblem(StatusCodes.Status400BadRequest)
+         .WithSummary("Create Or Update Basket")
+         .WithDescription("Store basket of the user for ecommerce microsservices");
+    }
 }
