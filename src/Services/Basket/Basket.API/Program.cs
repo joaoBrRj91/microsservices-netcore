48ff1e09103e77dab8a0fa1647737d856975390e
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-08-07 18:35:33 -0300

    HealthCheck backgroud services (redis and postgres) for basket microsservice

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,14 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 #region Add Services To The Container - DI
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCarterService(typeof(Program).Assembly, typeof(GetBasketEndpoint), typeof(StoreBasketEndpoint), typeof(DeleteBasketEndpoint));
+builder.Services.AddCarterService(
+    typeof(Program).Assembly,
+    typeof(GetBasketEndpoint),
+    typeof(StoreBasketEndpoint),
+    typeof(DeleteBasketEndpoint));
 
 builder.Services.AddMediatorWithFluentValidatorServices(typeof(Program).Assembly);
 
@@ -17,12 +24,16 @@ builder.Services.AddMarten(options =>
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.Decorate<IBasketRepository, CacheBasketRepository>();
+
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
    //options.InstanceName = "Basket";
 });
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
@@ -32,6 +43,12 @@ var app = builder.Build();
 
 //Use Common Services Added In Container -DI
 app.UseCommonServices();
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    }); ;
 
 app.Run();
 
