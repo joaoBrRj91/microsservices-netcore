4d737ceb141f6e6739fa1789eccd6122c71f793d
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-08-24 09:54:14 -0300

    Create Migrations pipeline sqllite

--- src/Services/Discount/Discount.GRPC/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.GRPC/Data/DiscountContext.cs
@@ -6,4 +6,14 @@ namespace Discount.GRPC.Data;
 internal class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
 {
     public DbSet<Coupon> Coupons { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        //Seed when call Add-Migration
+        modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id= Guid.NewGuid(), ProductName = "IPhone 15", Descriptiom = "IPhone Description", Amount= 499 },
+                new Coupon { Id = Guid.NewGuid(), ProductName = "IPhone 15", Descriptiom = "IPhone Description", Amount = 499 }
+            );
+    }
 }
--- src/Services/Discount/Discount.GRPC/Migrations/20240824123651_AddSeed.Designer.cs
+++ src/Services/Discount/Discount.GRPC/Migrations/20240824123651_AddSeed.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Discount.GRPC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Discount.GRPC.Migrations
 {
     [DbContext(typeof(DiscountContext))]
-    partial class DiscountContextModelSnapshot : ModelSnapshot
+    [Migration("20240824123651_AddSeed")]
+    partial class AddSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
