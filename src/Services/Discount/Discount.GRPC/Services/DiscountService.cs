eae23f04cc58b6a2a1e156f581d6029785f00556
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-08-24 11:47:40 -0300

    Delete Discount

--- src/Services/Discount/Discount.GRPC/Services/DiscountService.cs
+++ src/Services/Discount/Discount.GRPC/Services/DiscountService.cs
@@ -11,15 +11,16 @@ internal class DiscountService
     (DiscountContext discountContext, ILogger<DiscountService> logger)
     : DiscountProtoService.DiscountProtoServiceBase
 {
+
+    private const string BASE_INFO_LOG_SUCCESS = "Discount is {operation} for product : {productName}, amount: {amount}, description: {description}";
+
     public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
     {
-        var coupon = await discountContext
-            .Coupons
-            .FirstOrDefaultAsync(d => d.ProductName == request.ProductName);
+        var coupon = await FindCouponByProductName(request.ProductName);
 
         coupon ??= new Coupon { ProductName = request.ProductName, Description = "No Discount", Amount = 0 };
 
-        logger.LogInformation("Discount is retrieved for product : {productName}, amount: {amount}, description: {description}", coupon.ProductName, coupon.Amount, coupon.Description);
+        logger.LogInformation(BASE_INFO_LOG_SUCCESS, "retrived", coupon.ProductName, coupon.Amount, coupon.Description);
 
         return BuildCouponModelByCoupon(coupon);
     }
@@ -27,30 +28,63 @@ internal class DiscountService
     public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
     {
         var coupon = request.Coupon.Adapt<Coupon>();
-        if (coupon is null)
-            throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid Request"));
+
+        ThrowRpcExceptionIfInvalidCoupon(coupon, StatusCode.InvalidArgument, "Invalid Request");
 
         discountContext.Coupons.Add(coupon);
         await discountContext.SaveChangesAsync();
 
-        logger.LogInformation("Discount is created for product : {productName}, amount: {amount}, description: {description}", coupon.ProductName, coupon.Amount, coupon.Description);
+        logger.LogInformation(BASE_INFO_LOG_SUCCESS, "created", coupon.ProductName, coupon.Amount, coupon.Description);
 
         return BuildCouponModelByCoupon(coupon);
 
     }
 
-    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
     {
-        return base.UpdateDiscount(request, context);
+        var coupon = await discountContext
+            .Coupons
+            .FindAsync(request.Coupon.Id);
+
+        ThrowRpcExceptionIfInvalidCoupon(coupon, StatusCode.NotFound, "Not Found Coupon");
+
+        coupon = request.Coupon.Adapt(coupon)!;
+
+        discountContext.Coupons.Update(coupon);
+        await discountContext.SaveChangesAsync();
+
+        logger.LogInformation(BASE_INFO_LOG_SUCCESS, "updated", coupon.ProductName, coupon.Amount, coupon.Description);
+
+        return BuildCouponModelByCoupon(coupon);
     }
 
-    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
     {
-        return base.DeleteDiscount(request, context);
+        var coupon = await FindCouponByProductName(request.ProductName);
+
+        ThrowRpcExceptionIfInvalidCoupon(coupon, StatusCode.NotFound, "Not Found Coupon");
+
+        discountContext.Coupons.Remove(coupon!);
+        await discountContext.SaveChangesAsync();
+
+        logger.LogInformation(BASE_INFO_LOG_SUCCESS, "deleted", coupon!.ProductName, coupon.Amount, coupon.Description);
+
+        return new DeleteDiscountResponse { Success = true };
     }
 
 
-    private static CouponModel BuildCouponModelByCoupon(Coupon coupon) 
+    private async Task<Coupon?> FindCouponByProductName(string productName) 
+        => await discountContext
+           .Coupons
+           .FirstOrDefaultAsync(d => d.ProductName == productName);
+
+    private static CouponModel BuildCouponModelByCoupon(Coupon coupon)
         => new() { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount };
+
+    private static void ThrowRpcExceptionIfInvalidCoupon(Coupon? coupon, StatusCode status, string message = "Not informed")
+    {
+        if (coupon is null)
+            throw new RpcException(new Status(status, message));
+    }
 
 }
