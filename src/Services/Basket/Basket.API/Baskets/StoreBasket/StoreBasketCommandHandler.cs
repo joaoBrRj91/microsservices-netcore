70f24375f5108898d15f65dc73e2ae8c1e5542a4
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-08-28 21:23:53 -0300

    Add reference grpc service in basket api and initial logic call discont of product in store handler

--- src/Services/Basket/Basket.API/Baskets/StoreBasket/StoreBasketCommandHandler.cs
+++ src/Services/Basket/Basket.API/Baskets/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Baskets.StoreBasket;
+﻿using Discount.Grpc;
+
+namespace Basket.API.Baskets.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
 public record StoreBasketResult(bool IsSuccess, string UserName);
@@ -13,15 +15,30 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     }
 }
 
-public class StoreBasketCommandHandler(IBasketRepository basketRepository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+public class StoreBasketCommandHandler(
+    IBasketRepository basketRepository,
+    DiscountProtoService.DiscountProtoServiceClient discountClient) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
+        //TODO : Communicate with Discount.Grpc and calculate lastest prices of products into itens in cart
+        //TODO : Debt Tech - Create an grpc method for calculate discounts of a list of products
+        await DeductDiscount(discountClient, command);
 
         //TODO : Store basket in database (use Marten upsert - if exist = update, if not create)
         //TODO : Update Cache service with changes basket user 
         await basketRepository.StoreBasket(command.Cart, cancellationToken);
 
         return new StoreBasketResult(true, command.Cart.UserName);
+    }
+
+    private static async Task DeductDiscount(DiscountProtoService.DiscountProtoServiceClient discountClient, StoreBasketCommand command)
+    {
+        foreach (var item in command.Cart.Items)
+        {
+            var coupon = await discountClient.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName });
+            item.Price -= coupon.Amount;
+        }
+
     }
 }
