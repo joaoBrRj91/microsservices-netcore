698fb6d20c691375336624d7fc1429c2c71f0cb0
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-09-18 19:53:44 -0300

    Create base abstractions in domain layer

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions;
+
+public abstract class Entity<TIdentity> : IEntity<TIdentity>
+{
+    public abstract TIdentity Id { get; set; }
+    public abstract DateTime? CreateAt { get; set; }
+    public abstract string? CreateBy { get; set; }
+    public abstract DateTime? LastModified { get; set; }
+    public abstract string? LastModifiedBy { get; set; }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public interface IAggregate<TIdentity> : IEntity<TIdentity>
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    void AddDomainEvent(IDomainEvent domainEvent);
+    IDomainEvent[] ClearDomainEvents();
+}
