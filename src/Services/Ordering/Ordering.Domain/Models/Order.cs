4c91c52de2965340cd44244f80eb2b763619b883
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-10-02 21:44:25 -0300

    Finish rich entity domain

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -13,7 +13,57 @@ public class Order : Aggregate<OrderId>
     public OrderStatus OrderStatus { get; private set; }
 
     private readonly List<OrderItem> _orderItems = [];
-    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+    //Throw exception if user try force ToList()
+    public IReadOnlyCollection<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+
+    public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            OrderStatus = OrderStatus.Pending
+        };
+
+        //Add domain event in list of events for dispacher after trigger save changes in database
+
+        return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus orderStatus)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddress;
+        Payment = payment;
+        OrderStatus = orderStatus;
+
+        //Add domain event in list of events for dispacher after trigger save changes in database
+    }
+
+
+    public void AddOrderItem(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var orderItem = new OrderItem(Id, productId, price, quantity);
+        _orderItems.Add(orderItem);
+    }
+
+    public void RemoveOrderItem(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(o => o.ProductId == productId);
+        if(orderItem is not null) _orderItems.Remove(orderItem);
+
+        //throw domain exception if not find order item
+    }
 
     public void GenereteTotalPriceOrder() => TotalPrice = _orderItems.Sum(o => o.Price * o.Quantity);
 
--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -4,7 +4,7 @@ namespace Ordering.Domain.Models;
 
 public class OrderItem : Entity<OrderItemId>
 {
-    internal OrderItem(OrderId orderId, ProductId productId, decimal price, ushort quantity)
+    internal OrderItem(OrderId orderId, ProductId productId, decimal price, int quantity)
     {
         Id = OrderItemId.Of(Guid.NewGuid());
         OrderId = orderId;
@@ -16,6 +16,6 @@ public class OrderItem : Entity<OrderItemId>
     public OrderId OrderId { get; private set; }
     public ProductId ProductId { get; private set; }
     public decimal Price { get; private set; }
-    public ushort Quantity { get; private set; }
+    public int Quantity { get; private set; }
 
 }
