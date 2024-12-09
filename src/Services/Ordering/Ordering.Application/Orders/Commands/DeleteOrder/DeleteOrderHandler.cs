2570b88142d7e4af031cdc1fc2900542694e0186
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-12-09 20:35:31 -0300

    Delete order workflow

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,19 @@
+﻿using Ordering.Application.Exceptions;
+using Ordering.Domain.ValueObjects.TypesIds;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public sealed class DeleteOrderHandler(IAppDbContext dbContext)
+    : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        var order = await dbContext.Orders.FindAsync([OrderId.Of(command.OrderId)], cancellationToken)
+            ?? throw new OrderNotFoundException(command.OrderId);
+
+        dbContext.Orders.Remove(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteOrderResult(true);
+    }
+}
