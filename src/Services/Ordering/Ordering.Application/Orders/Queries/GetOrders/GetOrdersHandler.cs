58721204ac8cb3460108a88562ccf4c2446d9fc5
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-12-23 20:51:29 -0300

    Get orders with pagination query handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.Core.Pagination;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+internal class GetOrdersHandler(IAppDbContext appDbContext)
+    : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationRequest.PageIndex;
+        var pageSize = query.PaginationRequest.PageSize;
+
+        var totalOrders = await appDbContext.Orders.LongCountAsync(cancellationToken);
+
+        var orders = await appDbContext
+                .Orders
+                .AsNoTracking()
+                .OrderBy(o => o.OrderName)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+
+        return new GetOrdersResult(new PaginatedResult<OrderDto>
+            (pageIndex,
+            pageSize,
+            totalOrders,
+            orders.BuildOrdersDto()));
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.Core.Pagination;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public record GetOrdersQuery(PaginationRequest PaginationRequest) : IQuery<GetOrdersResult>;
+
+public record GetOrdersResult(PaginatedResult<OrderDto> PaginatedResult);
