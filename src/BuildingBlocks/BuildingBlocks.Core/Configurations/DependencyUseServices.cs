0f7a04aadee5d541111e4f0926088de03bddbc90
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-12-29 10:33:47 -0300

    Remove unecessary configuration exception. Have a global custom exception handler in building blocks

--- src/BuildingBlocks/BuildingBlocks.Core/Configurations/DependencyUseServices.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Configurations/DependencyUseServices.cs
@@ -10,9 +10,5 @@ public static class DependencyUseServices
         /*Find all class is have implementation ICarterModule (in assembly reigistrated) 
         * and map endpoints defined called AddRouter Method implemented from Interface*/
         app.MapCarter();
-
-
-        //Global Exception handler defined in BuildingBlocks for all microsservices
-        app.UseExceptionHandler(options => { });
     }
 }
