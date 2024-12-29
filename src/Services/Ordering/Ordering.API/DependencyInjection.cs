13640ad92b99c12e5ff5ccf118fdcb2306d68c4f
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-12-29 10:34:15 -0300

    Mapping endpoints for ordering api with carter

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,14 +1,26 @@
-﻿namespace Ordering.API;
+﻿using BuildingBlocks.Core.Configurations;
+using Carter;
+
+namespace Ordering.API;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
+        //services.AddCarter(
+        //                 typeof(Program).Assembly,
+        //                 typeof(CreateOrderEndpoint),
+        //                 typeof(DeleteOrderEndpoint),
+        //                 typeof(UpdateOrderEndpoint),
+        //                 typeof(GetOrdersEndpoint),
+        //                 typeof(GetOrdersByNameEndpoint),
+        //                 typeof(GetOrdersByCustomerEndpoint));
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app)
     {
+        app.UseCommonApiServices();
         return app;
     }
 }
