7ebc4d93b1b64a0250366934ef090e40a32e7bc5
joaoBrRj91 <joao1991algoritmo@gmail.com>
2025-01-20 20:28:24 -0300

    Add exception handler configuration in building blocks

--- src/BuildingBlocks/BuildingBlocks.Core/Configurations/DependencyUseServices.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Configurations/DependencyUseServices.cs
@@ -5,10 +5,19 @@ namespace BuildingBlocks.Core.Configurations;
 
 public static class DependencyUseServices
 {
-    public static void UseCommonApiServices(this WebApplication app)
+    public static void UseCommonApiServices(this WebApplication app,
+        bool isExceptionHandlerEnable = false,
+        Action<IApplicationBuilder>? configure = default)
     {
         /*Find all class is have implementation ICarterModule (in assembly reigistrated) 
         * and map endpoints defined called AddRouter Method implemented from Interface*/
         app.MapCarter();
+
+        //Add exception handler if is enable
+        if (isExceptionHandlerEnable)
+        {
+            var configureHandler = configure == default ? options => { } : configure;
+            app.UseExceptionHandler(configureHandler);
+        }
     }
 }
