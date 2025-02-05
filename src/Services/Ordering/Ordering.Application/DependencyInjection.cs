b3ebb4a97a65595c1c7a4de052c918c76a3deb9e
joaoBrRj91 <joao1991algoritmo@gmail.com>
2025-02-05 20:09:45 -0300

    Configuration feature flag for dispacher or not order created domain event to OrderFullfilment service

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using BuildingBlocks.Messaging.MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
 using System.Reflection;
 
 namespace Ordering.Application;
@@ -11,6 +12,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddMediatorWithFluentValidatorServices(Assembly.GetExecutingAssembly(), false);
+        services.AddFeatureManagement();
         services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
         return services;
     }
--- src/Services/Ordering/Ordering.Application/Orders/Events/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Events/Domain/OrderCreatedEventHandler.cs
@@ -1,13 +1,23 @@
-﻿namespace Ordering.Application.Orders.Events;
+﻿using MassTransit;
+using Microsoft.FeatureManagement;
 
-public sealed class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+namespace Ordering.Application.Orders.Events;
+
+public sealed class OrderCreatedEventHandler(
+    IPublishEndpoint publishEndpoint,IFeatureManager featureManager,
+    ILogger<OrderCreatedEventHandler> logger)
     : INotificationHandler<OrderCreateEvent>
 {
-    public Task Handle(OrderCreateEvent notification, CancellationToken cancellationToken)
+    public async Task Handle(OrderCreateEvent domainEvent, CancellationToken cancellationToken)
     {
-        var createNotification = notification as IDomainEvent;
+        var createNotification = domainEvent as IDomainEvent;
 
         logger.LogInformation($"Order event {nameof(OrderCreateEvent)} triggered by event type {createNotification.EventType} with event id {createNotification.EventId}");
-        return Task.CompletedTask;
+
+        if (await featureManager.IsEnabledAsync("OrderFullfilment"))
+        {
+            //var orderCreatedIntegrationEvent = domainEvent.Order.BuildOrderCreatedIntegrationEvent();
+            await publishEndpoint.Publish(new { Event = "order-created-integration-event" }, cancellationToken);
+        }
     }
 }
