cff254c998ad0112dd07215a327eb4ef5e579a21
joaoBrRj91 <joao1991algoritmo@gmail.com>
2025-03-24 21:36:02 -0300

    Create Shopping Web App

---
+++ src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Shopping.Web.Models.Catalog;
+
+internal class ProductModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public List<string> Category { get; set; } = new();
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }
+}
+
+//wrapper classes
+internal record GetProductsResponse(IEnumerable<ProductModel> Products);
+internal record GetProductByCategoryResponse(IEnumerable<ProductModel> Products);
+internal record GetProductByIdResponse(ProductModel Product);
